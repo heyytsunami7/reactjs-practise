fa3f23da1279d7cac0346bdc9371ebf2bb5a9644
heyytsunami7 <sakshamrana773@gmail.com>
2026-07-31 09:10:23 +0530

    small practise for input value

---
+++ live-echo-text/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+   const [text,setText] = useState("");
+
+  return (
+    <>
+      <input type="text" value={text} onChange={(e)=>{setText(e.target.value)}} />
+      <p>{text}</p>
+    </>
+  )
+}
+
+export default App
